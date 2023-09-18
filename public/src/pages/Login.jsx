a6317a03ad40d432d9b94eb3d2acf44c387f373a
heiswillz <wheeliams98@gmail.com>
2023-09-18 23:14:32 +0100

    first commit

---
+++ public/src/pages/Login.jsx
@@ -0,0 +1,41 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import styled from "styled-components";
+
+// const Login = () => {
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     alert("form");
+//   };
+
+//   const handleChange = ()=>{
+
+//   }
+
+//   return (
+//     <>
+//       <FormContainer>
+//         <form onSubmit={(e) => handleSubmit(e)}>
+//           <div className="brand">
+//             <img src="" alt=""></img>
+//             <h1>snappy</h1>
+//           </div>
+//           <input type="text" placeholder="Search" onChange={handleChange}/>
+//           <input type="text" placeholder="Search" onChange={handleChange}/>
+//         </form>
+//       </FormContainer>
+//     </>
+//   );
+// };
+
+// const FormContainer = styled.div``;
+
+// export default Login;
+
+import React from "react";
+
+const Login = () => {
+  return <div>Login</div>;
+};
+
+export default Login;
